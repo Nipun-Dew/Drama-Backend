f45a6766d54d9eecd7effb307faf4ab45e697d1a
Nipun-Dew <e16360@eng.pdn.ac.lk>
2021-07-30 01:48:40 +0530

    add drama routes

--- app.js
+++ app.js
@@ -12,56 +12,17 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopo
     console.log("Database is connected!");
 });
 
+// json body parser
 app.use(express.json());
 
+// testing endpoint
 app.get('/', (req, res) => {
     res.json({ "message": "Hello" });
 });
 
-app.get('/get/dramas', async(req, res) => {
-    try {
-        const data = await Drama.find({}).exec();
-        console.log(data);
-        res.json(data);
-    } catch (err) {
-        console.log(err);
-        res.json(err);
-    }
-});
-
-app.get('/get/comments', async(req, res) => {
-    try {
-        const data = await Drama.find({ _id: req.body._id }).exec();
-        console.log((data[0]).Comments);
-        res.json((data[0]).Comments);
-    } catch (err) {
-        console.log(err);
-        res.json(err);
-    }
-});
-
-app.post('/add/drama', async(req, res) => {
-    const drama = new Drama({
-        category: req.body.category,
-        title: req.body.title,
-        imageUrl: req.body.imageUrl,
-        description: req.body.description,
-        cast: req.body.cast,
-        Director: req.body.Director,
-        Producer: req.body.Producer,
-        genre: req.body.genre,
-        Comments: req.body.Comments,
-    });
-
-    try {
-        const result = await drama.save();
-        console.log(result);
-        res.json(result);
-    } catch (err) {
-        console.log(err);
-        res.json(err);
-    }
-});
+// add routes
+app.use('/drama', require('./routes/dramaRoutes'));
+//app.use('/user', require('./routes/userRoutes'));
 
 app.listen(3000, () => {
     console.log("Listen from port number 3000");
---
+++ routes/dramaRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+
+const Drama = require('../models/Drama');
+
+
+router.get('/get/dramas', async(req, res) => {
+    try {
+        const data = await Drama.find({}).exec();
+        console.log(data);
+        res.json(data);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+});
+
+router.get('/get/comments', async(req, res) => {
+    try {
+        const data = await Drama.find({ _id: req.body._id }).exec();
+        console.log((data[0]).Comments);
+        res.json((data[0]).Comments);
+        console.log("hello");
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+});
+
+router.post('/add/drama', async(req, res) => {
+    const drama = new Drama({
+        category: req.body.category,
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        description: req.body.description,
+        cast: req.body.cast,
+        Director: req.body.Director,
+        Producer: req.body.Producer,
+        genre: req.body.genre,
+        Comments: req.body.Comments,
+    });
+
+    try {
+        const result = await drama.save();
+        console.log(result);
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+});
+
+module.exports = router;
